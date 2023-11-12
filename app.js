0f63cdaef08a5a5c13fed9f3652a0bdf308012a3
Vidall <vidall.williams.web@gmail.com>
2023-11-12 13:32:50 -0500

    fixed issue where nodemon would not run because of hbs partial not called correctly

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const hbs = require('hbs')
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 const travelRouter = require("./app_server/routes/travel");
-const { hbs } = require("hbs");
 const expressHbs = require("express-handlebars");
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 // register handlebars partials(https://www.npmjs.com/package/hbs)
-hbs.registerPartial(path.join(__dirname, "app_server", "views/partials"));
+hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
 
 app.set("view engine", "hbs");
 
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/travel');
 
+router.get('./', controller.travel)
+
 module.exports = router;
